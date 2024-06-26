ba9d182b1a052b3d4365283fa64ead9c82a98caf
Evolução IT - Consultoria e serviços em tecnologia e informática - Maringá Paraná Brasil <106790903+evolucaoit@users.noreply.github.com>
2024-06-26 03:50:35 -0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -6,7 +6,7 @@ def main():
     root_dir = os.getcwd()
     
     # Define o arquivo que desejamos executar via Streamlit
-    py_file = "pyrefactor.py"
+    py_file = "git-ripperv3.py"
     
     # Comando para executar o Streamlit usando python3
     command = f'python3 -m streamlit run "{os.path.join(root_dir, py_file)}"'

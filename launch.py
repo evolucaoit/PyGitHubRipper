1d504a272f27efa04810a22f95026be7bbf90300
Evolução IT - Consultoria e serviços em tecnologia e informática - Maringá Paraná Brasil <106790903+evolucaoit@users.noreply.github.com>
2024-06-26 03:06:56 -0300

    Create launch.py

---
+++ launch.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+def main():
+    # Obtém o diretório onde este script está localizado
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Define o diretório onde queremos navegar (a raiz onde o script está)
+    root_dir = script_dir
+
+    # Abre o prompt de comando do Windows na pasta específica
+    subprocess.Popen(['cmd', '/K', 'cd', '/D', root_dir])
+
+    # Executa o arquivo git-ripperv3.py usando python -m streamlit run <arquivo>
+    selected_file = "git-ripperv3.py"
+    command = f'python -m streamlit run "{selected_file}"'
+    subprocess.Popen(command, cwd=root_dir, shell=True)
+
+if __name__ == "__main__":
+    main()

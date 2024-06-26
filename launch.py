da563df364978cb97f1d9a4fd969322a244607bd
Evolução IT - Consultoria e serviços em tecnologia e informática - Maringá Paraná Brasil <106790903+evolucaoit@users.noreply.github.com>
2024-06-26 04:01:30 -0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,18 +1,30 @@
 import os
 import subprocess
+import sys
 
 def main():
     # Obtém o diretório onde este script está localizado
     script_dir = os.path.dirname(os.path.abspath(__file__))
     
-    # Define o diretório onde queremos navegar (a raiz onde o script está)
+    # Define o diretório raiz onde o script git-ripperv3.py estará
     root_dir = script_dir
-
-    # Comando para executar o Streamlit usando python3
-    command = f'python3 -m streamlit run git-ripperv3.py'
-
-    # Executa o comando via subprocesso
-    subprocess.Popen(command, cwd=root_dir, shell=True)
+    
+    # Nome do script do Streamlit que será executado
+    streamlit_script = "git-ripperv3.py"
+    
+    try:
+        # Comando para executar o Streamlit usando python no Windows
+        if sys.platform.startswith('win'):
+            command = f'python -m streamlit run "{streamlit_script}"'
+        else:
+            # Comando para executar o Streamlit usando python em outros sistemas
+            command = f'python3 -m streamlit run "{streamlit_script}"'
+        
+        # Executa o comando via subprocesso
+        subprocess.Popen(command, cwd=root_dir, shell=True)
+    except Exception as e:
+        print(f"Erro ao tentar executar o Streamlit: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()

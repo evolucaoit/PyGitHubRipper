bd741239ca078ab8732f1163d79152042e20fd05
Evolução IT - Consultoria e serviços em tecnologia e informática - Maringá Paraná Brasil <106790903+evolucaoit@users.noreply.github.com>
2024-06-26 03:36:34 -0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -8,12 +8,12 @@ def main():
     # Define o diretório onde queremos navegar (a raiz onde o script está)
     root_dir = script_dir
 
-    # Abre o prompt de comando do Windows na pasta específica
+    # Abre o prompt de comando do Windows na pasta específica (opcional, pode não ser necessário)
     subprocess.Popen(['cmd', '/K', 'cd', '/D', root_dir])
 
     # Executa o arquivo git-ripperv3.py usando python -m streamlit run <arquivo>
     selected_file = "git-ripperv3.py"
-    command = f'python -m streamlit run "{selected_file}"'
+    command = f'python -m streamlit run "{os.path.join(root_dir, selected_file)}"'
     subprocess.Popen(command, cwd=root_dir, shell=True)
 
 if __name__ == "__main__":

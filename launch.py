30922c8041e45bf2b64a8ed245464476202a36d6
Evolução IT - Consultoria e serviços em tecnologia e informática - Maringá Paraná Brasil <106790903+evolucaoit@users.noreply.github.com>
2024-06-26 03:50:13 -0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,16 +1,15 @@
-import subprocess
-import sys
 import os
+import subprocess
 
 def main():
-    # Obtém o diretório onde este script está localizado
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Obtém o diretório atual onde este script está sendo executado
+    root_dir = os.getcwd()
     
-    # Define o diretório onde queremos navegar (a raiz onde o script está)
-    root_dir = script_dir
-
-    # Comando para executar o Streamlit via linha de comando
-    command = f'python3 -m streamlit run "{os.path.join(root_dir, "git-ripperv3.py")}"'
+    # Define o arquivo que desejamos executar via Streamlit
+    py_file = "pyrefactor.py"
+    
+    # Comando para executar o Streamlit usando python3
+    command = f'python3 -m streamlit run "{os.path.join(root_dir, py_file)}"'
 
     # Executa o comando via subprocesso
     subprocess.Popen(command, cwd=root_dir, shell=True)
